b13b6a01a1f418bd25839c795eca8a72fbf3f8c7
Mateus <mateusv251@gmail.com>
2022-05-24 21:57:39 -0300

    Rewrote stable vec as an option vec

--- src/structs/stable_vec.rs
+++ src/structs/stable_vec.rs
@@ -1,20 +1,13 @@
 use anyhow::Result;
-use bit_vec::BitVec;
 
 use crate::traits::serial::Serial;
 
 #[derive(Debug, Clone)]
-pub struct StableVec<T> {
-	vec: Vec<T>,
-	used: BitVec,
-}
+pub struct StableVec<T>(Vec<Option<T>>);
 
 impl<T> Default for StableVec<T> {
 	fn default() -> Self {
-		Self {
-			vec: Default::default(),
-			used: Default::default(),
-		}
+		Self(Vec::new())
 	}
 }
 
@@ -23,24 +16,19 @@ impl<T> StableVec<T> {
 		Self::default()
 	}
 	pub fn with_capacity(capacity: usize) -> Self {
-		Self {
-			vec: Vec::with_capacity(capacity),
-			used: BitVec::from_elem(capacity, false),
-		}
+		Self(Vec::with_capacity(capacity))
 	}
 	pub fn push(&mut self, item: T) -> usize {
-		let free = self.used.iter().position(|x| !x);
+		let free = self.0.iter().position(|x| x.is_none());
 		match free {
 			Some(index) => {
-				self.vec[index] = item;
-				self.used.set(index, true);
-				index
+        self.0.insert(index, Some(item));
+        index
 			}
-			None => {
-				let i = self.vec.len();
-				self.vec.push(item);
-				self.used.push(true);
-				i
+			_ => {
+        let i = self.0.len();
+				self.0.push(Some(item));
+        i
 			}
 		}
 	}
@@ -49,60 +37,52 @@ impl<T> StableVec<T> {
 		I: IntoIterator<Item = T> + ExactSizeIterator,
 	{
 		let free = self
-			.used
+			.0
 			.iter()
 			.enumerate()
-			.filter_map(|(i, x)| if x { None } else { Some(i) })
+			.filter_map(|(i, x)| if x.is_some() { None } else { Some(i) })
 			.collect::<Vec<_>>();
 		let to_insert = iter.len();
 		let to_reserve = {
 			let free_len = free.len();
 			to_insert.max(free_len) - free_len
 		};
-		self.vec.reserve(to_reserve);
-		self.used.reserve(to_reserve);
+		self.0.reserve(to_reserve);
 
 		let mut iter = iter.into_iter();
 		let mut indexes = Vec::with_capacity(to_insert);
 		for idx in free {
 			if let Some(item) = iter.next() {
-				self.used.set(idx, true);
-				self.vec.insert(idx, item);
+				self.0.insert(idx, Some(item));
 				indexes.push(idx);
 			} else {
 				break;
 			}
 		}
 		for item in iter {
-			let i = self.vec.len();
-			self.vec.push(item);
-			self.used.push(true);
-			indexes.push(i);
-		}
-		if self.used.len() < self.vec.len() {
-			eprintln!("Desync in extend");
+			let i = self.0.len();
+			self.0.push(Some(item));
+      indexes.push(i);
 		}
 		indexes
 	}
 	pub fn remove(&mut self, index: usize) {
-		self.used.set(index, false);
+		self.0.insert(index, None);
 	}
 	pub fn to_vec(&self) -> Vec<&T> {
-		self.vec
+		self.0
 			.iter()
-			.enumerate()
-			.filter_map(|(i, x)| if self.used[i] { Some(x) } else { None })
+			.filter_map(|x| x.as_ref())
 			.collect::<Vec<_>>()
 	}
 	pub fn into_vec(self) -> Vec<T> {
-		self.vec
+		self.0
 			.into_iter()
-			.enumerate()
-			.filter_map(|(i, x)| if self.used[i] { Some(x) } else { None })
+			.filter_map(|x| x)
 			.collect::<Vec<_>>()
 	}
 	pub fn len(&self) -> usize {
-		self.used.iter().filter(|x| *x).count()
+		self.0.iter().filter(|x| x.is_some()).count()
 	}
 	pub fn is_empty(&self) -> bool {
 		self.len() == 0
@@ -114,25 +94,29 @@ where
 	T: Serial,
 {
 	fn serialize(&self) -> Result<Vec<u8>> {
-		let mut items = Vec::with_capacity(self.vec.len());
-		for item in self.vec.iter() {
-			let item = Serial::serialize(item)?;
+		let mut items = Vec::with_capacity(self.0.len());
+		for item in self.0.iter() {
+      let item = match item {
+        Some(i) => Some(i.serialize()?),
+        None => None
+      };
+			let item = Serial::serialize(&item)?;
 			items.push(item);
 		}
-		let free = self.used.to_bytes();
-		items.push(free);
 		Serial::serialize(&items)
 	}
 
 	fn deserialize(bytes: Vec<u8>) -> Result<Self> {
-		let mut items: Vec<Vec<u8>> = Serial::deserialize(bytes)?;
-		let free = items.pop().unwrap();
+		let items: Vec<Vec<u8>> = Serial::deserialize(bytes)?;
 		let mut vec = Vec::with_capacity(items.len());
 		for item in items.into_iter() {
-			let item = Serial::deserialize(item)?;
+			let item: Option<Vec<u8>> = Serial::deserialize(item)?;
+      let item = match item {
+        Some(i) => Some(T::deserialize(i)?),
+        None => None
+      };
 			vec.push(item);
 		}
-		let free = BitVec::from_bytes(&free);
-		Ok(StableVec { vec, used: free })
+		Ok(StableVec(vec))
 	}
 }
